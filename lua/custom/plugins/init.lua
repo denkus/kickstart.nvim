6ee47b77ff8a1cd8e91eddc7b5e2648cd4ebb7c4
Denis Kustov <kustov.denic@gmail.com>
2026-04-04 18:59:05 +0700

    Add bufferline and bufremove plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,46 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = {
+      'echasnovski/mini.bufremove',
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    event = 'VeryLazy',
+    init = function()
+      vim.o.showtabline = 2
+    end,
+    opts = {
+      options = {
+        mode = 'buffers',
+        always_show_bufferline = true,
+        close_command = function(buffer_number) require('mini.bufremove').delete(buffer_number, false) end,
+        right_mouse_command = function(buffer_number) require('mini.bufremove').delete(buffer_number, false) end,
+        show_buffer_icons = vim.g.have_nerd_font,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = 'thin',
+        modified_icon = '[+]',
+      },
+    },
+  },
+  {
+    'echasnovski/mini.bufremove',
+    version = '*',
+    keys = {
+      {
+        '<leader>bd',
+        function() require('mini.bufremove').delete(0, false) end,
+        desc = '[B]uffer [D]elete',
+      },
+      {
+        '<leader>bD',
+        function() require('mini.bufremove').delete(0, true) end,
+        desc = '[B]uffer [D]elete!',
+      },
+    },
+  },
+}
