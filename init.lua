0650b763aa74a187c463d0fea75ddfcec6f919de
Denis Kustov <kustov.denic@gmail.com>
2026-04-09 15:02:42 +0700

    Add json formatter

--- init.lua
+++ init.lua
@@ -758,6 +758,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'jq' },
+        jsonc = { 'jq' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
